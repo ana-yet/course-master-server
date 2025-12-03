3f6684dc4b8b42591c14d30561cd962508b5ae3a
ana-yet <anayethusenaj@gmail.com>
2025-12-03 15:42:20 +0600

    mongodb connection solved

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 import dotenv from "dotenv";
+dotenv.config();
+
 import connectDB from "./src/config/db.js";
 import { app } from "./src/app.js";
-
-dotenv.config({
-  path: "./.env",
-});
 
 connectDB()
   .then(() => {
