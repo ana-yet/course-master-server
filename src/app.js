3b0378fe9f415d036a2d96b09003825a3c7a5c66
ana-yet <anayethusenaj@gmail.com>
2025-12-03 15:33:39 +0600

    created app.js file and imported global error and used helmet for secure http and origin selected

---
+++ src/app.js
@@ -0,0 +1,30 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import helmet from "helmet";
+import morgan from "morgan";
+
+const app = express();
+
+//  Global Middlewares
+app.use(helmet()); // Secure HTTP
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+app.use(cookieParser());
+app.use(morgan("dev")); // Log requests
+
+// 2. Import Routes (We will create these next)
+// import userRouter from './routes/user.routes.js';
+// app.use("/api/v1/users", userRouter);
+
+// Global Error Handler
+import { errorHandler } from "./middlewares/error.middleware.js";
+app.use(errorHandler);
+
+export { app };
