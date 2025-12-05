caaa3788076a51fc5359a7ba5f8d26357a22e6cc
ana-yet <anayethusenaj@gmail.com>
2025-12-05 10:41:00 +0600

    removed cors

--- src/app.js
+++ src/app.js
@@ -8,14 +8,7 @@ const app = express();
 
 //  Global Middlewares
 app.use(helmet()); // Secure HTTP
-app.use(
-  cors({
-    origin:
-      "https://ana-yet-course-master-client.vercel.app" ||
-      "http://localhost:3000",
-    credentials: true,
-  })
-);
+app.use(cors());
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
