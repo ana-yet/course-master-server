090ab4c6ad34723c9f5e114f097c71fd76b6851c
ana-yet <anayethusenaj@gmail.com>
2025-12-05 10:54:30 +0600

    fixed the cors issue

--- src/app.js
+++ src/app.js
@@ -8,7 +8,13 @@ const app = express();
 
 //  Global Middlewares
 app.use(helmet()); // Secure HTTP
-app.use(cors());
+app.use(
+  cors({
+    origin: "https://ana-yet-course-master-client.vercel.app",
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+  })
+);
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
