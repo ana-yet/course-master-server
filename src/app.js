38cb900f1919c8634e5a377b60bdcb96f5b0bfbe
ana-yet <anayethusenaj@gmail.com>
2025-12-05 10:37:47 +0600

    updated

--- src/app.js
+++ src/app.js
@@ -17,8 +17,6 @@ app.use(
   })
 );
 
-console.log(process.env.CORS_ORIGIN);
-
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
